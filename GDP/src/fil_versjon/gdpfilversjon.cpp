9893bb752cf2476e2acd8239e465fb88c6b7e1a7
halftan <halftan@saetherskar.no>
2015-03-19 11:52:14 +0100

    nop

---
+++ GDP/src/fil_versjon/gdpfilversjon.cpp
@@ -0,0 +1,5 @@
+#include "gdpfilversjon.h"
+
+GDPFilversjon::GDPFilversjon()
+{
+}
---
+++ GDP/src/fil_versjon/gdpfilversjon.h
@@ -0,0 +1,10 @@
+#ifndef GDPFILVERSJON_H
+#define GDPFILVERSJON_H
+
+class GDPFilversjon
+{
+public:
+    GDPFilversjon();
+};
+
+#endif // GDPFILVERSJON_H
