559d0876d59029501cc592104b28497d3b670ac5
halftan <halftan@saetherskar.no>
2015-03-23 15:16:13 +0100

    nop

--- GDPLib/src/modell.h
+++ GDPLib/src/modell.h
@@ -1,23 +1,12 @@
-/*
-** This file is part of the GDPLib project.
-**
-** Copyright (C) Halftan Sætherskar (halftan@saetherskar.no)
-**
-** This program is free software; you can redistribute it and/or
-** modify it under the terms of the GNU General Public License
-** as published by the Free Software Foundation; either version 2
-** of the License, or (at your option) any later version.
-**
-** This program is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with this program; if not, write to the Free Software
-** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-******************************************************************************
-*/
+/*! \file modell.h
+    \brief GDPL modell    
+    
+    Definerer datamodellen til GDPL, samt funksjonalitet for å skrive og lese 
+    denne modellen til og fra ei fil. 
+    
+    Datamodellen består av ei lenka liste av konkurransenoder. Hver konkurransenode 
+    har ei lenka liste med person-noder og ei lenka liste med par-noder.
+*/    
 
 #ifndef MODELL_H
 #define MODELL_H
@@ -26,18 +15,52 @@
 extern "C" {
 #endif
 
+/*! \def GDPL_MAX_PERSONNAVN_LENGDE
+    \brief Max antall tegn et person-navn kan være.
+    
+    Med tegn menes her 'unsigned int'. Et UTF-8 encoded tegn kan være opp til fire slike 'tegn'.
+*/
 #define GDPL_MAX_PERSONNAVN_LENGDE           128
+/*! \def GDPL_MIN_PERSONNAVN_LENGDE
+    \brief Minimum antall tegn en person-navn må være.
+*/
 #define GDPL_MIN_PERSONNAVN_LENGDE           1
+/*! \def GDPL_MAX_FILNAVN_LENGDE
+    \brief Max antall tegn et fil-navn kan være.
+    
+    Med tegn menes her 'unsigned int'. Et UTF-8 encoded tegn kan være opp til fire slike 'tegn'.
+*/
 #define GDPL_MAX_FILNAVN_LENGDE              516
+/*! \def GDPL_MIN_FILNAVN_LENGDE
+    \brief Minimum antall tegn en person-navn må være.
+*/
 #define GDPL_MIN_FILNAVN_LENGDE              1
+/*! \def GDPL_MAX_ANTALL_PAR
+    \brief Max antall par som det er mulig å registrere.
+    
+    I forbindelse med logikk som skal trekke et tilfeldig tall, må bruker av 
+    denne koden definere et tak, eller vindu, som det skal trekkes innenfor.
+    Denne verdien angir en max-verdi for dette vinduet.
+*/
 #define GDPL_MAX_ANTALL_PAR                  200
 
+/*! \struct GDPL_tid
+    \brief Definerer atributtene til en tid.   
+    
+    Benyttes som dataholder ifm registrering av start- og måltid, anvendt tid
+    og middeltid.    
+*/
 struct GDPL_tid {
     int timer;
     int minutt;
     int sekund;
 };
 
+/*! \struct GDPL_par_data_node_struct
+    \brief Definerer atributtene til et par.   
+    
+    TODO: forklar ...
+*/
 struct GDPL_par_data_node_struct {
     int id;
     int herre_person_id;
@@ -51,6 +74,11 @@ struct GDPL_par_data_node_struct {
     struct GDPL_par_data_node_struct *neste;
 };
 
+/*! \struct GDPL_person_data_node_struct
+    \brief Definerer atributtene til en person.   
+    
+    TODO: forklar ...
+*/
 struct GDPL_person_data_node_struct {
     int id;    
     char fnavn[GDPL_MAX_PERSONNAVN_LENGDE];
@@ -58,9 +86,21 @@ struct GDPL_person_data_node_struct {
     struct GDPL_person_data_node_struct *neste;
 };
 
+/*! \var typedef struct GDPL_par_data_node_struct GDPL_par_data_node
+    \brief En typedefinisjon av GDPL_par_data_node
+*/
 typedef struct GDPL_par_data_node_struct GDPL_par_data_node;
+
+/*! \var typedef struct GDPL_person_data_node_struct GDPL_person_data_node
+    \brief En typedefinisjon av GDPL_person_data_node
+*/
 typedef struct GDPL_person_data_node_struct GDPL_person_data_node;
 
+/*! \struct GDPL_konkurranse_data_node_struct
+    \brief Definerer atributtene til en konkurranse.   
+    
+    TODO: forklar ...
+*/
 struct GDPL_konkurranse_data_node_struct {
     int id;
     int aar;
@@ -69,25 +109,59 @@ struct GDPL_konkurranse_data_node_struct {
     struct GDPL_konkurranse_data_node_struct *neste;
 };
 
+/*! \var typedef struct GDPL_konkurranse_data_node_struct GDPL_konkurranse_data_node
+    \brief En typedefinisjon av GDPL_konkurranse_data_node
+*/
 typedef struct GDPL_konkurranse_data_node_struct  GDPL_konkurranse_data_node;
 
-/* Variabler */
-
-
+/*! \var extern GDPL_konkurranse_data_node *gdpl_modell_konkurranseliste_root_ptr;
+    \brief Definerer root-pekeren for hele datastrukturen.    
+*/
 extern GDPL_konkurranse_data_node *gdpl_modell_konkurranseliste_root_ptr;
 
+/*! \var extern GDPL_konkurranse_data_node *gdpl_modell_konkurranseliste_valgt_ptr;
+    \brief Definerer den valgte konkurransen. Det vil si den konkurransen 'vi jobber med'. 
+*/
 extern GDPL_konkurranse_data_node *gdpl_modell_konkurranseliste_valgt_ptr;
 
-/* Funksjons-deklareringer */
 
+/*! \fn int GDPL_modell_angi_filnavn(const char *filnavn)
+    \brief Angir \a filnavnet som modellen skal bruke som persistent lager.
+    \param filnavn Et filnavn. Om denne pekeren er null, benyttes et default filnavn.
+*/
 int GDPL_modell_angi_filnavn(const char *filnavn);
 
+/*! \fn int GDPL_modell_les_data()
+    \brief Les inn modellen fra fil.
+    \return Feilkode i form av en \a int. Om denne er ulik null, har en feil inntruffet. 
+    
+    Leser inn modellen fra fil. Om fila ikke eksisterer, opprettes denne, og en
+    ny tom modell klargjøres til bruk.
+*/
 int GDPL_modell_les_data();
 
+/*! \fn int GDPL_modell_skriv_data()
+    \brief Skriver modellen til fil.
+    \return Feilkode i form av en \a int. Om denne er ulik null, har en feil inntruffet. 
+    
+    TODO: beskriv feilkoder ...
+*/
 int GDPL_modell_skriv_data();
 
+/*! \fn int GDPL_modell_dump()
+    \brief Dumper modellen til log.
+    \return Feilkode i form av en \a int. Om denne er ulik null, har en feil inntruffet. 
+    
+    Log-nivå må være større eller lik 'info'.    
+*/
 int GDPL_modell_dump();
 
+/*! \fn int GDPL_modell_nullstill()
+    \brief Nullstiller en eksisterende modell.
+    \return Feilkode i form av en \a int. Om denne er ulik null, har en feil inntruffet. 
+    
+    Nullstiller eller sletter en modell.
+*/
 int GDPL_modell_nullstill();
 
 #ifdef __cplusplus
